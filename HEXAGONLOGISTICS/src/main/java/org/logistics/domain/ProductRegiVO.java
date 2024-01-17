e6f48a3214b865a21d264a77d9f13bf6ce2b246f
hexagon <hexagon@DESKTOP-NVGRHKM>
2024-01-17 17:47:57 +0900

    asdf

---
+++ HEXAGONLOGISTICS/src/main/java/org/logistics/domain/ProductRegiVO.java
@@ -0,0 +1,27 @@
+package org.logistics.domain;
+
+ 
+import java.util.Date;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Setter
+@Getter
+public class ProductRegiVO {
+	private String p_name;
+	private int p_seoul;
+	private int p_suwon;
+	private int p_incheon;
+	private int price;
+	/* private Date date; */ 
+	/* private String writer; */
+	
+	
+	
+	 
+	
+
+}
